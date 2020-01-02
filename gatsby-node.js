368f47de157268919cd450990f1282ef4e3176eb
Patrick Lee <cluster000@gmail.com>
2020-01-02 20:57:47 +0800

    Initial commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,74 @@
  */
 
 // You can delete this file if you're not using it
+
+const path = require("path")
+
+const makeRequest = (graphql, request) =>
+  new Promise((resolve, reject) => {
+    resolve(
+      graphql(request).then(result => {
+        if (result.errors) {
+          reject(result.errors)
+        }
+
+        return result
+      })
+    )
+  })
+
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions
+
+  const getArticles = makeRequest(
+    graphql,
+    `
+    {
+      allStrapiArticle {
+        edges {
+          node {
+            id
+          }
+        }
+      }
+    }
+  `
+  ).then(result => {
+    result.data.allStrapiArticle.edges.forEach(({ node }) => {
+      createPage({
+        path: `/${node.id}`,
+        component: path.resolve(`src/templates/article.js`),
+        context: {
+          id: node.id,
+        },
+      })
+    })
+  })
+
+  const getAuthors = makeRequest(
+    graphql,
+    `
+    {
+      allStrapiUser {
+        edges {
+          node {
+            id
+          }
+        }
+      }
+    }
+  `
+  ).then(result => {
+    result.data.allStrapiUser.edges.forEach(({ node }) => {
+      createPage({
+        path: `/authors/${node.id}`,
+        component: path.resolve(`src/templates/author.js`),
+        context: {
+          id: node.id,
+        },
+      })
+    })
+  })
+
+  return Promise.all([getArticles, getAuthors])
+}
